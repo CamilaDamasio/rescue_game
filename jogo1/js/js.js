95f17758dae77743777fe9eec6279d4f844dbf66
CamilaDamasio <camiladamasioef@hotmail.com>
2022-03-17 09:40:22 -0300

    Adiciona dificuldade ao abater inimigo

--- jogo1/js/js.js
+++ jogo1/js/js.js
@@ -178,6 +178,7 @@ function start() {
     // Disparo com o inimigo1
     if (colisao3.length > 0) {
       pontos = pontos + 100;
+      velocidade = velocidade + 0.3;
       inimigo1X = parseInt($("#inimigo1").css("left"));
       inimigo1Y = parseInt($("#inimigo1").css("top"));
 
